553cc2847f5b183637e0c554237dc3bc477c0da4
alidogan0716 <alidogan0716@gmail.com>
2021-10-01 01:51:22 +0300

    0.1.0

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_mvvm_text/view/home/view-model/user_view_model.dart';
+import 'view/home/view/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  UserViewModel userViewModel = Get.put(UserViewModel());
-
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Obx(
-          () => Container(
-            child: userViewModel.userState == UserState.BUSY
-                ? const Center(child: CircularProgressIndicator())
-                : userViewModel.userState == UserState.ERROR
-                    ? const Center(child: Text("Error"))
-                    : Center(
-                        child: Text(
-                            "Adı : ${userViewModel.userList[0].name} Soyadı : ${userViewModel.userList[0].surname} Yaşı : ${userViewModel.userList[0].age}"),
-                      ),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
